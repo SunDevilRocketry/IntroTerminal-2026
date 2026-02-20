8c890583e2bf5b4628024d2502a7990c0d57ec34
Ljmeza95 <ljmeza95@gmail.com>
2026-02-20 16:01:32 -0700

    made updates to branch consistent with requests made in the project comment. There may still be style errors...

--- CommandLineInterface.py
+++ CommandLineInterface.py
@@ -1,34 +1,65 @@
-class commands:
+class CLI:
     documentation = {}
     def __init__(self):
+        self.documentation = {
+             "dummy": "A dummy argument to test the functionality of the help function",
+             "dummy3": "Another dummy argument to test the functionality of our code",
+             "help": "List of Available Commands, or one specific command"
+        }
         pass
-    def dummy(arg):
-        print("dummy print")
-    documentation["dummy"] = "A dummy argument to test the functionality of the help function"
-    def getdocumentation(key):
-        return commands.documentation[key]
-command_prompts = {} #Initial command serialization
-
-def help(arg):
+    
+    def getdocumentation(self,key):
+        return self.documentation[key]
+    
+    def help(self,arg):
         #no argument case: list all functions
         if len(arg) == 0: 
-             print(f"placeholder :D")
+            keys = list(command_prompts.keys())
+            
+            print(f"\nAvailable Command Prompts: \n")
+            for k in keys:
+                print(f"{k}    ----    {self.getdocumentation(k)}")
         #1 argument case: list 
-        if len(arg) == 1:
-             print(f"{arg[0]}:\n{commands.getdocumentation(arg[0])}")
-command_prompts["help"] = help
-command_prompts["dummy"] = commands.dummy
+        elif len(arg) == 1:
+            print(f"{arg[0]}    ----    {self.getdocumentation(arg[0])}")
+        else:
+            print("Error: incorrect argument count.\n")
+        print("")
+        
+class DUMMY:
+    def __init__(self):
+        pass
+    def dummy(self):
+        print("dummy print")
+    def dummy3(self,arg):
+        if len(arg) != 3:
+            print("Error: incorrect argument count.\n\n Please Enter 3 arguments")
+        else:
+            print(f"{arg[0]} {arg[1]} {arg[2]}")
+
+cli = CLI()
+dumdum = DUMMY()
+
+command_prompts ={
+     "help":cli.help,
+     "dummy":dumdum.dummy,
+     "dummy3":dumdum.dummy3
+    }
+
+
 #ask for user input
 #if input corresponds to key, run function stored at key values
 
+print("Welcome to LeOS")
+
 while True: # Super-Loop
-    userInput = input('')
+    userInput = input('LeOS>> ')
     #split command into arguments
     args = userInput.split(' ')
     command = args[0]
     args.remove(args[0])
     if command == "exit":
-        print("Exiting Program...")
+        print("Thank You for Using LeOS")
         break
     #run the function at the location of the hash
     command_prompts[command](args)
