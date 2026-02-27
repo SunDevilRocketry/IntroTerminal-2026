be2878ae32abeb22615c04a116e0b57cadb6660a
fallencoffee <aaron.tsang@hotmail.com>
2026-02-27 13:17:40 -0700

    Update terminalserial.py

--- terminalserial.py
+++ terminalserial.py
@@ -1,29 +1,44 @@
 #terminalserial.py
 import serial
+import serial.tools.list_ports
+
 
 class SerialTerminal:
-    def __init__(self, comport, baudrate,numbytes):
+    def __init__(self, comport = None, baudrate = None, timeout = None):
         self.__comport = comport
         self.__baudrate = baudrate
-        serialinfo = None
-        read_test = serial.SerialException
-        self.__numbytes = numbytes
-        if read_test == "":
-            print(serial.tools.list_ports.comports(include_links=False))
-        else:
-            print(serial.SerialException)
-            
-            #direct pyserial call avail COM ports, gather all available in a list and returns
-            #create an if statement to check for connection issues before function call
+        self.__timeout = timeout
+        self.serialinfo = None
 
-        def read(num_bytes):
-            #pyserial read
-            return serialinfo.read(num_bytes)
+    def read(self, num_bytes):
+        #pyserial read
+        return self.serialinfo.read(num_bytes)
+    
+    def write(self, write_byte):
+        #pyserial write
+        self.serialinfo.write(write_byte)
+    
+    def connect(self, comport ,baudrate):
+        #open com ports
+        open(comport,baudrate)
+
+        self.serialinfo = serial.Serial(comport, baudrate)
+
+    def get_available_ports(self):
+        ports = serial.tools.list_ports.comports()
+        return ports
+
+    def getSerialObject(self):
+        return self.serialinfo
+    
+    def setComport(self, comport):
+        self.__comport = comport
+    
+    def getComport(self):
+        return self.__comport
+    
+    def setBaudrate(self, baudrate):
+        self.__baudrate = baudrate\
         
-        def write(write_byte):
-            #pyserial write
-            serialinfo.write(write_byte)
-        
-        def connect(comport,baudrate):
-            #open com ports
-            open(comport,baudrate)
+    def getBaudrate(self):
+        return self.__baudrate
